018ad4e9c2bb66529c77d0c7d2ba1ff9ddfff469
Jayhind-Navik <jayhindnavik7052@gmail.com>
2025-07-29 13:52:06 +0530

    first commit

--- BookShoppingCartMvcUI/Program.cs
+++ BookShoppingCartMvcUI/Program.cs
@@ -7,6 +7,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+Console.WriteLine("Connecting to SQL Server with connection string:");
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
